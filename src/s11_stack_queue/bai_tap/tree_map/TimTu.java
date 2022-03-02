91e13e8a79dc28b2f6ee5f7ee7358cedc46e8cf8
buitin99 <buiquoctin.job@gmail.com>
2022-03-02 21:39:58 +0700

    nop bai ss12

---
+++ src/s11_stack_queue/bai_tap/tree_map/TimTu.java
@@ -0,0 +1,15 @@
+package s11_stack_queue.bai_tap.tree_map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TreeMap {
+    Map<String,Integer> map = new HashMap<>();
+
+    map
+    map.put("J",1);
+    map.put("J",1);
+    map.put("J",1);
+    map.put("J",1);
+
+}
--- src/s11_stack_queue/bai_tap/tree_map/TreeMap.java
+++
@@ -1,6 +0,0 @@
-package s11_stack_queue.bai_tap.tree_map;
-
-public class TreeMap {
-
-
-}
