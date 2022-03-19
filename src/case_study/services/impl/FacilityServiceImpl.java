50536948316351193b9a8192c8190518782e741f
buitin99 <buiquoctin.job@gmail.com>
2022-03-19 21:45:37 +0700

    nop bai cace study

--- src/case_study/services/impl/FacilityServiceImpl.java
+++ src/case_study/services/impl/FacilityServiceImpl.java
@@ -1,4 +1,5 @@
 package case_study.services.impl;
+
 import case_study.models.*;
 import case_study.services.IFacilityService;
 import case_study.utils.ReadAndWriteCSV;
@@ -11,7 +12,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class FacilityServiceImpl implements IFacilityService {
-    private static Map<Facility,Integer> facilityIntegerMap = new LinkedHashMap<>();
+    private static Map<Facility, Integer> facilityIntegerMap = new LinkedHashMap<>();
     private static Scanner scanner = new Scanner(System.in);
     static List<House> readAndWriteHouseToCSV = ReadAndWriteCSV.readHouseListFromCSV();
     static List<Room> readAndWriteRoomToCSV = ReadAndWriteCSV.readRoomListFromCSV();
@@ -20,6 +21,7 @@ public class FacilityServiceImpl implements IFacilityService {
     public static final String REG_VL = "^SVVL-\\d{4}$";
     public static final String REG_RO = "^SVRO-\\d{4}$";
     public static final String REG_HO = "^SVHO-\\d{4}$";
+    public static final String SERVICE_NAME = "^[A-Z][a-z]{2,}$";
 
 
     @Override
@@ -30,7 +32,7 @@ public class FacilityServiceImpl implements IFacilityService {
         System.out.println("3. ADD NEW ROOM!");
         System.out.println("4. RETURN TO MAIN MENU!");
         int choose = Integer.parseInt(scanner.nextLine());
-        switch (choose){
+        switch (choose) {
             case 1:
                 addNewVilla();
                 break;
@@ -45,19 +47,19 @@ public class FacilityServiceImpl implements IFacilityService {
 
     @Override
     public void display() {
-        for (Villa v: readAndWriteVillaToCSV) {
-            facilityIntegerMap.put(v,0);
+        for (Villa v : readAndWriteVillaToCSV) {
+            facilityIntegerMap.put(v, 0);
         }
 
-        for (House h: readAndWriteHouseToCSV) {
-            facilityIntegerMap.put(h,0);
+        for (House h : readAndWriteHouseToCSV) {
+            facilityIntegerMap.put(h, 0);
         }
 
-        for (Room r: readAndWriteRoomToCSV) {
-            facilityIntegerMap.put(r,0);
+        for (Room r : readAndWriteRoomToCSV) {
+            facilityIntegerMap.put(r, 0);
         }
 
-        for (Map.Entry<Facility,Integer> e : facilityIntegerMap.entrySet()) {
+        for (Map.Entry<Facility, Integer> e : facilityIntegerMap.entrySet()) {
             System.out.println("Service:" + e.getKey() + "Used:" + e.getValue());
         }
     }
@@ -73,96 +75,167 @@ public class FacilityServiceImpl implements IFacilityService {
         System.out.println("Enter service code!");
         String vSC = inputVilla();
         System.out.println("Enter service name!");
-        String vSN = inputVilla();
-        System.out.println("Enter area used!");
-        String vAU = scanner.nextLine();
+        String vSN = inputServiceName();
+        String vAU;
+        boolean flag = true;
+        do {
+            System.out.println("Enter area used!");
+            vAU = scanner.nextLine();
+            double checkV = Double.parseDouble(vAU);
+            if (checkV > 30) {
+                flag = false;
+            }
+        } while (flag);
         System.out.println("Enter rental Fee!");
         String vRF = scanner.nextLine();
-        System.out.println("Enter max number of people!");
-        String vMax = scanner.nextLine();
+        String vMax;
+        boolean flag2 = true;
+        do {
+            System.out.println("Enter max number of people!");
+            vMax = scanner.nextLine();
+            int checkM = Integer.parseInt(vMax);
+            if (checkM > 0 && checkM < 20) {
+                flag2 = false;
+            }
+        } while (flag2);
         System.out.println("Enter type of rental!");
         String vTOR = scanner.nextLine();
         System.out.println("Enter room standard!");
         String vRS = scanner.nextLine();
-        System.out.println("Enter pool area!");
-        String vPA = scanner.nextLine();
-        System.out.println("Enter number of floors!");
-        String vNOF = scanner.nextLine();
-        Villa villa = new Villa(vSC,vSN,vAU,vRF,vMax,vTOR,vRS,vPA,vNOF);
-        facilityIntegerMap.put(villa,0);
+        String vPA;
+        boolean flag1 = true;
+        do {
+            System.out.println("Enter pool area!");
+            vPA = scanner.nextLine();
+            double checkPA = Double.parseDouble(vPA);
+            if (checkPA > 30) {
+                flag1 = false;
+            }
+        } while (flag1);
+        String vNOF;
+        boolean flag3 = true;
+        do {
+            System.out.println("Enter number of floors!");
+            vNOF = scanner.nextLine();
+            double checkF = Double.parseDouble(vNOF);
+            if (checkF > 0) {
+                flag3 = false;
+            }
+        } while (flag3);
+        Villa villa = new Villa(vSC, vSN, vAU, vRF, vMax, vTOR, vRS, vPA, vNOF);
+        facilityIntegerMap.put(villa, 0);
         readAndWriteVillaToCSV.add(villa);
-        ReadAndWriteCSV.writeVilla(readAndWriteVillaToCSV,true);
-
+        ReadAndWriteCSV.writeVilla(readAndWriteVillaToCSV, true);
     }
 
     @Override
     public void addNewHouse() {
         System.out.println("Enter service code!");
-        String hSC = scanner.nextLine();
+        String hSC = inputHouse();
         System.out.println("Enter service name!");
-        String hSN = scanner.nextLine();
+        String hSN = inputServiceName();
         System.out.println("Enter area used!");
-        String hAU = scanner.nextLine();
+        String hAU;
+        boolean flag = true;
+        do {
+            System.out.println("Enter area used!");
+            hAU = scanner.nextLine();
+            double checkAU = Double.parseDouble(hAU);
+            if (checkAU > 30) {
+                flag = false;
+            }
+        } while (flag);
         System.out.println("Enter rental Fee!");
         String hRF = scanner.nextLine();
         System.out.println("Enter max number of people!");
-        String hMax = scanner.nextLine();
+        String hMax;
+        boolean flag2 = true;
+        do {
+            System.out.println("Enter max number of people!");
+            hMax = scanner.nextLine();
+            double checkM = Double.parseDouble(hMax);
+            if (checkM > 0 && checkM < 20) {
+                flag2 = false;
+            }
+        } while (flag2);
         System.out.println("Enter type of rental!");
         String hTOR = scanner.nextLine();
         System.out.println("Enter room standard!");
         String hRS = scanner.nextLine();
         System.out.println("Enter number of floors!");
-        String hNOF = scanner.nextLine();
-        House house = new House(hSC,hSN,hAU,hRF,hMax,hTOR,hRS,hNOF);
-        facilityIntegerMap.put(house,0);
+        String hNOF;
+        boolean flag3 = true;
+        do {
+            System.out.println("Enter number of floors!");
+            hNOF = scanner.nextLine();
+            double checkF = Double.parseDouble(hNOF);
+            if (checkF > 0) {
+                flag3 = false;
+            }
+        } while (flag3);
+        House house = new House(hSC, hSN, hAU, hRF, hMax, hTOR, hRS, hNOF);
+        facilityIntegerMap.put(house, 0);
         readAndWriteHouseToCSV.add(house);
-        ReadAndWriteCSV.writeHouse(readAndWriteHouseToCSV,true);
+        ReadAndWriteCSV.writeHouse(readAndWriteHouseToCSV, true);
     }
 
     @Override
     public void addNewRoom() {
         System.out.println("Enter service code!");
-        String rSC = scanner.nextLine();
+        String rSC = inputRoom();
         System.out.println("Enter service name!");
-        String rSN = scanner.nextLine();
+        String rSN = inputServiceName();
         System.out.println("Enter area used!");
-        String rAU = scanner.nextLine();
+        String rAU;
+        boolean flag = true;
+        do {
+            System.out.println("Enter area used!");
+            rAU = scanner.nextLine();
+            double checkAU = Double.parseDouble(rAU);
+            if (checkAU > 30) {
+                flag = false;
+            }
+        } while (flag);
         System.out.println("Enter rental Fee!");
         String rRF = scanner.nextLine();
         System.out.println("Enter max number of people!");
-        String rMax = scanner.nextLine();
+        String rMax;
+        boolean flag2 = true;
+        do {
+            System.out.println("Enter max number of people!");
+            rMax = scanner.nextLine();
+            double checkM = Double.parseDouble(rMax);
+            if (checkM > 0 && checkM < 20) {
+                flag2 = false;
+            }
+        } while (flag2);
         System.out.println("Enter type of rental!");
         String rTOR = scanner.nextLine();
         System.out.println("Enter free service");
         String rFS = scanner.nextLine();
-        Room room = new Room(rSC,rSN,rAU,rRF,rMax,rTOR,rFS);
-        facilityIntegerMap.put(room,0);
+        Room room = new Room(rSC, rSN, rAU, rRF, rMax, rTOR, rFS);
+        facilityIntegerMap.put(room, 0);
         readAndWriteRoomToCSV.add(room);
-        ReadAndWriteCSV.writeRoom(readAndWriteRoomToCSV,true);
+        ReadAndWriteCSV.writeRoom(readAndWriteRoomToCSV, true);
     }
 
-    private String inputVilla(){
+    private String inputVilla() {
         System.out.println("ENTER VILLA SERVICE");
-        return RegexData.regexStr(scanner.nextLine(),REG_VL,"WRONG SERVICE VILLA : SVVL-NNNN. EX SVVL-1234!");
+        return RegexData.regexStr(scanner.nextLine(), REG_VL, "WRONG SERVICE VILLA : SVVL-NNNN. EX SVVL-1234!");
     }
 
-    private String inputRoom(){
+    private String inputRoom() {
         System.out.println("ENTER ROOM SERVICE ");
-        return RegexData.regexStr(scanner.nextLine(),REG_VL,"WRONG SERVICE ROOM : SVRO-NNNN. EX SVRO-1234!");
+        return RegexData.regexStr(scanner.nextLine(), REG_VL, "WRONG SERVICE ROOM : SVRO-NNNN. EX SVRO-1234!");
     }
 
-    private String inputHouse(){
+    private String inputHouse() {
         System.out.println("ENTER HOUSE SERVICE ");
-        return RegexData.regexStr(scanner.nextLine(),REG_VL,"WRONG SERVICE ROOM : SVHO-NNNN. EX SVHO-1234!");
+        return RegexData.regexStr(scanner.nextLine(), REG_VL, "WRONG SERVICE ROOM : SVHO-NNNN. EX SVHO-1234!");
     }
 
-    private String inputServiceName(){
+    private String inputServiceName() {
         System.out.println("ENTER SERVICE NAME");
-        return RegexData.regexStr(scanner.nextLine(),REG_VL,"WRONG NAME SERVICE : EX Villa!");
+        return RegexData.regexStr(scanner.nextLine(), SERVICE_NAME, "WRONG NAME SERVICE : EX Villa!");
     }
-
-//    private String serviceName(){
-//        System.out.println("ENTER SERVICE NAME");
-//        return RegexData.regexStr(scanner.nextLine(),)
-//    }
 }
